a79ff7726883d564180d1e3f119f66bdbe60c3fd
agalan8 <alejandrogalancaballero@hotmail.com>
2025-04-03 17:39:56 +0200

    Cambio de nombre del modelo Categoria a Tag

--- app/Policies/TagPolicy.php
+++ app/Policies/TagPolicy.php
@@ -2,11 +2,11 @@
 
 namespace App\Policies;
 
-use App\Models\Categoria;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class CategoriaPolicy
+class TagPolicy
 {
     /**
      * Determine whether the user can view any models.
@@ -19,7 +19,7 @@ class CategoriaPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Categoria $categoria): bool
+    public function view(User $user, Tag $tag): bool
     {
         return false;
     }
@@ -35,7 +35,7 @@ class CategoriaPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Categoria $categoria): bool
+    public function update(User $user, Tag $tag): bool
     {
         return false;
     }
@@ -43,7 +43,7 @@ class CategoriaPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Categoria $categoria): bool
+    public function delete(User $user, Tag $tag): bool
     {
         return false;
     }
@@ -51,7 +51,7 @@ class CategoriaPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Categoria $categoria): bool
+    public function restore(User $user, Tag $tag): bool
     {
         return false;
     }
@@ -59,7 +59,7 @@ class CategoriaPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Categoria $categoria): bool
+    public function forceDelete(User $user, Tag $tag): bool
     {
         return false;
     }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AdminMiddleware;
@@ -31,7 +31,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('users', UserController::class)->middleware(AdminMiddleware::class);
-Route::resource('categorias', CategoriaController::class)->middleware(AdminMiddleware::class);
+Route::resource('tags', TagController::class)->middleware(AdminMiddleware::class);
 
 Route::post('/images/update', function (Request $request) {
     $request->validate([
