3e8b143a2b619857335af15e41597d2e373dcfd2
agalan8 <alejandrogalancaballero@hotmail.com>
2025-04-03 10:00:27 +0200

    [RF] Gestión de redes sociales admin - Creación de tabal pivote socials_users, CRUD Social

--- database/migrations/2025_04_03_073556_create_social_user_table.php
+++ database/migrations/2025_04_03_073556_create_social_user_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('socials', function (Blueprint $table) {
-            $table->id();
+        Schema::create('social_user', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('social_id')->constrained();
+            $table->string('perfil');
+            $table->primary(['user_id', 'social_id']);
             $table->timestamps();
-            $table->string('nombre');
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('socials');
+        Schema::dropIfExists('social_user');
     }
 };
